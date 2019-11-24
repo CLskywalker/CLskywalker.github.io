e771337a5fc3a2b9c63a9036f2d330291a81d0e7
CLskywalker <clskywalker@outlook.com>
2019-11-24 12:23:02 +0800

    Site updated: 2019-11-24 12:23:02

--- dist/music.js
+++ dist/music.js
@@ -1,22 +1,10 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    autoplay: true,
-    loop: 'all',
-    volume: 0.7,
-    listFolded: true,
-    listMaxHeight: 60,
-    audio: [
-        {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
-        },
-        {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
-        }
-    ]
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: 'http://music.163.com/song/media/outer/url?id=3441491828.mp3',
+        cover: 'cover.jpg',
+    }]
 });
