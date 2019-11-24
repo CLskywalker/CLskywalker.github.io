d6f25fa0b4d1bebc1f51ee4052e16d2637dc1541
CLskywalker <clskywalker@outlook.com>
2019-11-24 12:07:16 +0800

    Site updated: 2019-11-24 12:07:16

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: '水星记',
+        artist: '郭顶',
+        url: 'http://music.163.com/song/media/outer/url?id=ID441491828.mp3',
+        cover: 'http://p1.music.126.net/wSMfGvFzOAYRU_yVIfquAA==/2946691248081599.jpg?param=130y130',
+    }]
+});
