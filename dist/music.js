f88a7007eb57cb491ded174c7462607c48c20680
CLskywalker <clskywalker@outlook.com>
2019-11-25 12:53:06 +0800

    Site updated: 2019-11-25 12:53:06

--- dist/music.js
+++ dist/music.js
@@ -8,20 +8,18 @@ const ap = new APlayer({
         artist: '郭顶',
         url: 'http://music.163.com/song/media/outer/url?id=441491828.mp3',
         cover: 'http://p1.music.126.net/wSMfGvFzOAYRU_yVIfquAA==/2946691248081599.jpg?param=130y130',
-      }
-      
-      {
-        name: "富士山下",
-        artist: '陈奕迅',
-        url: 'http://music.163.com/song/media/outer/url?id=65766.mp3',
-        cover: 'http://p2.music.126.net/PcJq6i7zMcPgudejdn1yeg==/109951163256302356.jpg?param=130y130',
-      }
-
+      },      
       {
         name: "Traveling light",
         artist: 'Joel Hanson',
         url: 'http://music.163.com/song/media/outer/url?id=1491585.mp3',
         cover: 'http://p2.music.126.net/zYqptzyHld2ylpb5FOoqDA==/1785606883508227.jpg?param=130y130',
+      }, 
+      {
+        name: "富士山下",
+        artist: '陈奕迅',
+        url: 'http://music.163.com/song/media/outer/url?id=65766.mp3',
+        cover: 'http://p2.music.126.net/PcJq6i7zMcPgudejdn1yeg==/109951163256302356.jpg?param=130y130',
       }
     ]
 });
